40be03fa6b0aa8793c655cf4a6832c395399e3be
Rain Liu <liuwenbin168@126.com>
2018-12-23 19:39:05 +0800

    Added DrawerLayout and NavigationView

--- WanAndroid/app/src/main/java/com/chinalwb/wanandroid/MainActivity.java
+++ WanAndroid/app/src/main/java/com/chinalwb/wanandroid/MainActivity.java
@@ -3,6 +3,8 @@ package com.chinalwb.wanandroid;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -21,13 +23,29 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        this.setSupportActionBar(toolbar);
+        initToolbar();
         initFragment();
+    }
+
+    private void initToolbar() {
+        this.setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.nav_open_drawer,
+                R.string.nav_close_drawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
     private void initFragment() {
